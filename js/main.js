f20dc287800c38fd092c74cc00bba516230793d0
kn1feR26rus <kn1feR26rus@yandex.ru>
2023-10-04 20:06:14 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -8,16 +8,43 @@ $(document).ready(function () {
     $(".payment__tooltip").toggleClass("active");
   });
 
-  $('.payment__form-submit').click(function() {
-    if ($('.payment__terms-checkbox').is(':checked')) {
-        $('.payment__terms').removeClass('alert');
+  $(".payment__form-submit").click(function () {
+    if ($(".payment__terms-checkbox").is(":checked")) {
+      $(".payment__terms").removeClass("alert");
     } else {
-        $('.payment__terms').addClass('alert');
+      $(".payment__terms").addClass("alert");
     }
   });
 
-  $('.payment__btn').click(function() {
-    $('.payment__btn').removeClass('active')
-    $(this).addClass('active');
-  })
+  $(".payment__btn").click(function () {
+    $(".payment__btn").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#credit-btn").click(function () {
+    $(".box-template").hide();
+    $(".payment__form-container").fadeIn();
+  });
+
+  $("#gift-btn").click(function () {
+    $(".box-template").hide();
+    $(".payment__gift-box").fadeIn();
+  });
+
+  $("#paypal-btn").click(function () {
+    $(".box-template").hide();
+    $(".payment__paypal-box").fadeIn();
+  });
+
+  $(".payment__terms-btn").click(function () {
+    $(".background").show();
+    $(".modal").show();
+    $("body, html").addClass("overflow");
+  });
+
+  $(".modal__close, .background").click(function () {
+    $(".background").hide();
+    $(".modal").hide();
+    $("body, html").removeClass("overflow");
+  });
 });
